0be327e3eb31216bbd6cc5309b277985a4a215d8
ade8m <benammaradem83@gmail.com>
2022-12-28 12:29:06 +0100

    fix a rout problem using routing annotations.

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -6,14 +6,29 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+
 class QuestionController
 {
+        /**
+        * @Route("/" )
+        */
+    
+    
     public function homepage()
     {
-        #[Route('/')]
-
-
+        
         return new Response('this is first page symfony');
     }
+
+     /**
+     * @Route("/questions/How to use symfony")
+     */
+    public function show()
+    {
+        return new Response('for use symfony we must undrestand this cours');
+        
+    }
+
 }
 ?>                                                            ?
